4045130044914ae68a056c069f245c07ee6edff6
Alex <carnagechef@live.co.uk>
2025-07-02 20:55:40 +0100

    adds drawer

--- lib/screens/coin_flipper_screen.dart
+++ lib/screens/coin_flipper_screen.dart
@@ -53,7 +53,8 @@ class _coin_flipper_screenState extends State<coin_flipper_screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flip a Coin')),
+      appBar:
+          AppBar(title: const Text('A very dull web apo'), centerTitle: true),
       drawer: StatsDrawer(onSelect: (t, c) => _showDetails(context, t, c)),
 
       // 1️⃣ StreamBuilder to fetch heads/tails counts in real time
